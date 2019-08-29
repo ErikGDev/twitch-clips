8cf0f331fbf4b4c104cbf5bee1e94db0d5cc7b93
Erik Goesmann <erikgoesmann@gmail.com>
2019-08-29 14:31:49 +1200

    Visual error fixed. Videos of all sizes now concatenate properly

--- src/concatenate.py
+++ src/concatenate.py
@@ -13,7 +13,7 @@ lines = clip_file.readlines()
 for line in lines:
     list_of_clips.append(VideoFileClip(line.strip()))
 
-final_clip = concatenate_videoclips(list_of_clips)
+final_clip = concatenate_videoclips(list_of_clips, method='compose')
 
 final_clip.write_videofile("top_fortnite_clips_" + str(today) + ".mp4")
 
